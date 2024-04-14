54dc3eaf51a83dc4b7a446770ba1380e2a0afddf
amilpa <amilpa2020@gmail.com>
2024-04-15 00:55:00 +0530

    styling

--- app/result/page.js
+++ app/result/page.js
@@ -1,13 +1,15 @@
 "use client";
-import ResultCard from "@/components/ResultCard";
+import ResultCard from "@/components/ResultCard/ResultCard";
 import supabase from "@/lib/supabase";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
+import styles from "./page.module.css";
+
 export default function Home() {
   const router = useRouter();
-  const [layatharangResults , setLayatharangResults] = useState([]);
-  const [chakravyuhResults , setChakravyuhResults] = useState([]);
+  const [layatharangResults, setLayatharangResults] = useState([]);
+  const [chakravyuhResults, setChakravyuhResults] = useState([]);
 
   /*
     typeOf results
@@ -27,20 +29,22 @@ export default function Home() {
       ]
     }
     */
-  const [loading , setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-  useEffect(()=>{
+  useEffect(() => {
     setLoading(true);
-    async function fetchResults(){
+    async function fetchResults() {
       const chakRes = await supabase
-      .from('event')
-      .select('*, results: result(* , house(name))')
-      .eq('domain', "CHAKRAVYUH").order('created_at', { ascending: false });
+        .from("event")
+        .select("*, results: result(* , house(name))")
+        .eq("domain", "CHAKRAVYUH")
+        .order("created_at", { ascending: false });
       const layaRes = await supabase
-      .from('event')
-      .select('*, results: result(*)')
-      .eq('domain', "LAYATARANG").order('created_at', { ascending: false });
-      
+        .from("event")
+        .select("*, results: result(*)")
+        .eq("domain", "LAYATARANG")
+        .order("created_at", { ascending: false });
+
       if (layaRes.error) console.log(layaRes.error);
       if (chakRes.error) console.log(chakRes.error);
       console.log(chakRes.data[0]);
@@ -49,23 +53,21 @@ export default function Home() {
       setLoading(false);
     }
     fetchResults();
-  } , []);
+  }, []);
   return (
     <main>
-      <h2 className="my-12 text-center text-3xl text-white">Results</h2>
-      <div className="mb-8 grid grid-cols-1 place-items-center gap-16 md:grid-cols-3">
-        <ResultCard />
-        <ResultCard />
-        <ResultCard />
-        <ResultCard />
-        <ResultCard />
-        <ResultCard />
-        <ResultCard />
-        <ResultCard />
-        <ResultCard />
-        <ResultCard />
-        <ResultCard />
-        <ResultCard />
+      <div className={styles.container}>
+        <h2 className={styles.text}>Results</h2>
+      </div>
+      <div className="mb-8 grid grid-cols-1 place-items-center gap-y-16 md:grid-cols-3">
+        {chakravyuhResults.map((result) => (
+          <ResultCard key={result.id} result={result} />
+        ))}
+        <ResultCard result={{ name: "Poem writing" }} />
+        <ResultCard result={{ name: "Writing competition" }} />
+        <ResultCard result={{ name: "Swimming competition" }} />
+        <ResultCard result={{ name: "Poem writing" }} />
+        <ResultCard result={{ name: "Poem writing" }} />
       </div>
     </main>
   );
