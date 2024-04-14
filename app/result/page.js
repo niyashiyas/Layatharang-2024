f7652171befe645fbf521ff3e7a871748f8ee5b3
vaidyanath b <b.vaidyanath003@gmail.com>
2024-04-14 18:48:02 +0530

    fetch results in public page

--- app/result/page.js
+++ app/result/page.js
@@ -1,10 +1,55 @@
 "use client";
 import ResultCard from "@/components/ResultCard";
+import supabase from "@/lib/supabase";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const router = useRouter();
+  const [layatharangResults , setLayatharangResults] = useState([]);
+  const [chakravyuhResults , setChakravyuhResults] = useState([]);
 
+  /*
+    typeOf results
+    {
+      id: number
+      name: string   //event Name
+      domain: string //CHAKRAVYUH or LAYATARANG
+      results: [
+        {
+          id: number
+          house: {
+            name: string
+          }
+          position: number
+          participant: string // single string for all participants
+        }
+      ]
+    }
+    */
+  const [loading , setLoading] = useState(true);
+
+  useEffect(()=>{
+    setLoading(true);
+    async function fetchResults(){
+      const chakRes = await supabase
+      .from('event')
+      .select('*, results: result(* , house(name))')
+      .eq('domain', "CHAKRAVYUH").order('created_at', { ascending: false });
+      const layaRes = await supabase
+      .from('event')
+      .select('*, results: result(*)')
+      .eq('domain', "LAYATARANG").order('created_at', { ascending: false });
+      
+      if (layaRes.error) console.log(layaRes.error);
+      if (chakRes.error) console.log(chakRes.error);
+      console.log(chakRes.data[0]);
+      setLayatharangResults(layaRes.data);
+      setChakravyuhResults(chakRes.data);
+      setLoading(false);
+    }
+    fetchResults();
+  } , []);
   return (
     <main>
       <h2 className="my-12 text-center text-3xl text-white">Results</h2>
