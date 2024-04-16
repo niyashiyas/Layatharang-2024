743323917fd5861f872260e90423d6ed7ac2aedf
Niya Shiyas <niyashiyas03@gmail.com>
2024-04-16 18:53:27 +0530

    ui changes

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
     <main className="flex h-screen flex-col items-center justify-between gap-4 bg-black	object-cover p-24">
       <div className="absolute -mt-28 h-5/6 w-full bg-[url('/group9.png')] bg-contain bg-center bg-no-repeat sm:-mt-24 sm:h-4/5"></div>
       <div className=" mt-80 flex h-full flex-col items-center justify-center sm:mt-96">
-        <div className="mt-2 flex rounded-3xl bg-[#151515] font-semibold sm:mt-12 md:mt-8">
+        <div className="mt-2 flex rounded-3xl bg-[#151515] font-semibold sm:mt-8 md:mt-5">
           <div
             onClick={() => router.push("scoreboard")}
             className="cursor-pointer rounded-3xl bg-[#fdb43f] px-5 py-1 text-black hover:bg-[#ffa71a] sm:px-8 sm:py-2"
